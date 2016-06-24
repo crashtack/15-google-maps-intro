ff55b054840c7d528d9c22b209e18194b736dda0
Rick Patci <rpatci@gmail.com>
2016-06-24 07:30:56 -0700

    push gmaps starter

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 (function(module) {
 
-  var styleArray = [
+  var stylesArray = [
     {
       featureType: "all",
       stylers: [
@@ -25,32 +25,5 @@
     }
   ];
 
-var mapOptions = {
-  zoom: 15,
-  styles: styleArray,
-  center: new google.maps.LatLng(47.618217, -122.351832),
-  mapTypeId: google.maps.MapTypeId.STREET,
-  zoomControl: true,
-  zoomControlOptions: {
-    position: google.maps.ControlPosition.RIGHT_CENTER
-  }
-}
-
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-
-google.maps.event.addDomListener(window, 'resize', function() {
-  var center = map.getCenter();
-  google.maps.event.trigger(map, 'resize');
-  map.setCenter(center);
-});
-
-
-
-
-
-
-
-
-
-})();
+module.map = map;
+})(window);
