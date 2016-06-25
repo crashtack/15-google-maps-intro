098174c3aaa10670b4785153508c27ab6bb4504a
crashtack <crashtack@gmail.com>
2016-06-24 21:57:49 -0700

    moved firebase init script to its own file firebaseConfig.js

---
+++ public/firebaseConfig.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD_yMtkI6CNN6o8k1FaHEUh9jRx343nYKQ",
+  authDomain: "crashtack-301-test.firebaseapp.com",
+  databaseURL: "https://crashtack-301-test.firebaseio.com",
+  storageBucket: "crashtack-301-test.appspot.com",
+};
+firebase.initializeApp(config);
