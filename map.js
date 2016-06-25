e104390563c22924fff7496714edd95d4c1e8560
crashtack <crashtack@gmail.com>
2016-06-24 20:48:45 -0700

    after inclass edits

--- map.js
+++ map.js
@@ -25,5 +25,29 @@
     }
   ];
 
+  var mapOptions = {
+    zoom: 15,
+    styles: stylesArray,
+    center: new google.maps.LatLng(47.618217, -122.351832),
+    mapTypeID: google.maps.MapTypeId.STREET,
+    zoomControl: true,
+    ZoomControlOptions: {
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    }
+  }
+
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  google.maps.event.addDomListener(window, 'resize', function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(center);
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 47.618217, lng: -122.351832},
+    map: map
+  });
+
 module.map = map;
 })(window);
